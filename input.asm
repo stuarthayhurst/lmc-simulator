542f603a65110251378e5c5698a0b9678d5c8d64
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-03-30 12:31:03 +0100

    Add a sample assembly file

---
+++ input.asm
@@ -0,0 +1,19 @@
+#Multiply 2 inputs
+INP
+STA num1
+INP
+STA num2
+loop LDA result
+ADD num1
+STA result
+LDA num2
+SUB one
+STA num2
+BRP loop
+LDA result
+OUT
+HLT
+one DAT 1
+num1 DAT 0
+num2 DAT 0
+result DAT 0
