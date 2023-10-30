32c812f4880ffa851b548816ee2e04782d39d4d0
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2023-10-30 23:27:07 +0000

    Simplify check for opcode 900 when validating memory addresses

--- src/simulator.hpp
+++ src/simulator.hpp
@@ -77,7 +77,7 @@ bool executeNextInstruction(bool* success) {
     }
 
     //Check operand address is within bounds (exception for I/O, as it's not an address)
-    if (opcode / 100 != 9 && checkMemoryAddress(&systemState, operand) == -1) {
+    if (opcode != 900 && checkMemoryAddress(&systemState, operand) == -1) {
       std::cout << "B" << std::endl;
       *success = true;
       return false;
