f835bc44b2e74f2cc7cab5a2aa893fade621ea5b
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2024-12-21 00:23:42 +0000

    Restructure code to use object files

--- src/common/instructions.hpp
+++ src/common/instructions.hpp
@@ -1,124 +1,27 @@
 #ifndef INSTRUCTIONS
 #define INSTRUCTIONS
 
-#include <iostream>
+#include <map>
 
 #include "state.hpp"
 
+typedef int (*InstructionHandler)(SystemState*, int);
+
 namespace instructions {
-  namespace {
-    int applyOverflow(int value) {
-      //If the value is above or below 999 or -999, wrap to other side of the interval
-      if (value > 999) {
-        value -= 999;
-        value = -999 + (value - 1);
-      } else if (value < -999) {
-        value += 999;
-        value = 999 + (value + 1);
-      }
-
-      return value;
-    }
-  }
-
-  int add(SystemState* systemState, int operand) {
-    systemState->accumulator += (systemState->memoryPtr)[operand];
-    systemState->accumulator = applyOverflow(systemState->accumulator);
-    return 0;
-  }
-
-  int subtract(SystemState* systemState, int operand) {
-    systemState->accumulator -= (systemState->memoryPtr)[operand];
-    systemState->accumulator = applyOverflow(systemState->accumulator);
-    return 0;
-  }
-
-  int store(SystemState* systemState, int operand) {
-    (systemState->memoryPtr)[operand] = systemState->accumulator;
-    return 0;
-  }
-
-  int load(SystemState* systemState, int operand) {
-    systemState->accumulator = (systemState->memoryPtr)[operand];
-    return 0;
-  }
-
-  int branchAlways(SystemState* systemState, int operand) {
-    systemState->programCounter = operand;
-    return 0;
-  }
-
-  int branchZero(SystemState* systemState, int operand) {
-    if (systemState->accumulator == 0) {
-      systemState->programCounter = operand;
-    }
-    return 0;
-  }
-
-  int branchPositive(SystemState* systemState, int operand) {
-    if (systemState->accumulator >= 0) {
-      systemState->programCounter = operand;
-    }
-    return 0;
-  }
-
-  int inputOutput(SystemState* systemState, int operand) {
-    if (operand == 1) { //Input
-      std::cin >> systemState->accumulator;
-    } else if (operand == 2) { //Output
-      std::cout << systemState->accumulator << std::endl;
-    } else {
-      std::cerr << "ERROR: Unknown opcode '" << 900 + operand << "'" << " at address " \
-                << systemState->programCounter - 1 << std::endl;
-      return -1;
-    }
-    return 0;
-  }
+  int add(SystemState* systemState, int operand);
+  int subtract(SystemState* systemState, int operand);
+  int store(SystemState* systemState, int operand);
+  int load(SystemState* systemState, int operand);
+  int branchAlways(SystemState* systemState, int operand);
+  int branchZero(SystemState* systemState, int operand);
+  int branchPositive(SystemState* systemState, int operand);
+  int inputOutput(SystemState* systemState, int operand);
 }
 
 namespace instructions {
-  typedef int (*InstructionHandler)(SystemState*, int);
-  std::map<int, InstructionHandler> opcodeHandlerMap = {
-    {100, instructions::add},
-    {200, instructions::subtract},
-    {300, instructions::store},
-    {500, instructions::load},
-    {600, instructions::branchAlways},
-    {700, instructions::branchZero},
-    {800, instructions::branchPositive},
-    {900, instructions::inputOutput}
-  };
-
-  struct OpcodeData {
-    int opcode;
-    bool requiresOperand;
-  };
-  std::map<std::string, int> mnemonicOpcodeMap = {
-    {"DAT", 000},
-    {"HLT", 000},
-    {"ADD", 100},
-    {"SUB", 200},
-    {"STA", 300},
-    {"LDA", 500},
-    {"BRA", 600},
-    {"BRZ", 700},
-    {"BRP", 800},
-    {"INP", 901},
-    {"OUT", 902}
-  };
-  std::map<std::string, int> mnemonicOperandMap = {
-    {"DAT", true},
-    {"HLT", false},
-    {"ADD", true},
-    {"SUB", true},
-    {"STA", true},
-    {"LDA", true},
-    {"BRA", true},
-    {"BRZ", true},
-    {"BRP", true},
-    {"INP", false},
-    {"OUT", false}
-  };
+  extern std::map<int, InstructionHandler> opcodeHandlerMap;
+  extern std::map<std::string, int> mnemonicOpcodeMap;
+  extern std::map<std::string, int> mnemonicOperandMap;
 }
 
 #endif
--- src/simulator.hpp
+++ src/simulator.hpp
@@ -1,106 +1,11 @@
-#include <iostream>
+#ifndef SIMULATOR
+#define SIMULATOR
 
 #include "common/state.hpp"
-#include "common/instructions.hpp"
 
-namespace {
-  int checkMemoryAddress(SystemState* systemState, int address) {
-    if (address >= systemState->memoryLength) {
-      std::cerr << "ERROR: Cannot access memory address '" << address << "'" << std::endl;
-      return -1;
-    }
-    return 0;
-  }
-}
+SystemState* createSimulator(int memoryLength);
+void destroySimulator(SystemState* systemState);
+void printMemory(SystemState* systemState, int size);
+bool executeNextInstruction(SystemState* systemState, bool* finished);
 
-SystemState* createSimulator(int memoryLength) {
-  //Create simulator
-  SystemState* systemState = new (std::nothrow) SystemState;
-  if (systemState == nullptr) {
-    return nullptr;
-  }
-
-  //Initialise system state values
-  systemState->accumulator = 0;
-  systemState->programCounter = 0;
-  systemState->memoryLength = memoryLength;
-
-  //Create simulator memory
-  systemState->memoryPtr = new (std::nothrow) int[systemState->memoryLength]{0};
-  if (systemState->memoryPtr == nullptr) {
-    return nullptr;
-  }
-
-  //Warn on non-100 memory sizes
-  if (systemState->memoryLength != 100) {
-    std::cerr << "WARNING: Memory set to non-standard value, behaviour may be altered" \
-              << std::endl;
-  }
-
-  return systemState;
-}
-
-void destroySimulator(SystemState* systemState) {
-  delete [] systemState->memoryPtr;
-  delete systemState;
-}
-
-//Print n memory locations
-void printMemory(SystemState* systemState, int size) {
-  for (int i = 0; i < size; i++) {
-    std::cout << systemState->memoryPtr[i] << std::endl;
-  }
-  std::cout << std::endl;
-}
-
-bool executeNextInstruction(SystemState* systemState, bool* finished) {
-    //Check memory address is within bounds
-    if (checkMemoryAddress(systemState, systemState->programCounter) == -1) {
-      //Invalid address, end program early
-      *finished = false;
-      return false;
-    }
-
-    //Get opcode and operand
-    int operand = systemState->memoryPtr[systemState->programCounter] % 100;
-    int opcode = systemState->memoryPtr[systemState->programCounter] - operand;
-
-    //Increment the program counter
-    systemState->programCounter++;
-
-    //Stop execution if told to halt
-    if (opcode == 0) {
-      //Success, program ended
-      *finished = true;
-      return true;
-    }
-
-    //Check operand address is within bounds (exception for I/O, as it's not an address)
-    if (opcode != 900 && checkMemoryAddress(systemState, operand) == -1) {
-      //Invalid address, end program early
-      std::cerr << "ERROR: Invalid operand '" << operand << "' at address " \
-                << systemState->programCounter - 1 << std::endl;
-      *finished = false;
-      return false;
-    }
-
-    //Retreive and execute 'instruction'
-    if (instructions::opcodeHandlerMap.contains(opcode)) {
-      instructions::InstructionHandler handler = instructions::opcodeHandlerMap[opcode];
-      if (handler(systemState, operand) == -1) {
-        //Failed to execute, end program early
-        *finished = false;
-        return false;
-      }
-    } else {
-      //Failed to execute, end program early
-      std::cerr << "ERROR: Unknown opcode '" << opcode << "' at address " \
-                << systemState->programCounter - 1 << std::endl;
-      *finished = false;
-      return false;
-    }
-
-  //Success, continue execution
-  *finished = false;
-  return true;
-}
+#endif
