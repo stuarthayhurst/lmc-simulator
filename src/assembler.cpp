477f114fddbdc2bc929b2e9780823b014e94fc4a
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2025-10-12 14:47:14 +0100

    Move tokenisation to a spearate function

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -7,6 +7,15 @@
 #include "common/instructions.hpp"
 
 namespace {
+  struct CodeData {
+    int lineNumber;
+    std::string instruction;
+    std::string operandLabel = "NONE";
+    int operandData = 0;
+  };
+}
+
+namespace {
   static std::vector<std::string> splitLine(std::string lineText) {
     std::vector<std::string> codeVector;
     bool lastCharacterWasSpace = true;
@@ -39,6 +48,82 @@ namespace {
     }
     return codeVector;
   }
+
+  /*
+   - Convert a vector of lines of code into tokens
+   - Build a label address map at the same time
+   - Returns true if successful
+  */
+  static bool tokeniseCode(std::vector<std::string>* code, std::vector<CodeData>* tokens,
+                           std::unordered_map<std::string, int>* labelAddressMap) {
+    std::vector<std::string> pendingLabels;
+    int lineAddress = 0;
+
+    /*
+     - Generate a token for each line of code, skipping comments and whitespace
+     - Add label declarations to pendingLabels, and convert them to an address
+       when a code line is found
+    */
+    for (unsigned int i = 0; i < code->size(); i++) {
+      //Split the line into words, skip comments
+      std::vector<std::string> lineVector = splitLine((*code)[i]);
+      if (lineVector.size() == 0) {
+        continue;
+      }
+
+      //If the first word isn't an instruction, assume it's a label
+      if (!instructions::mnemonicOpcodeMap.contains(lineVector[0])) {
+        //Use the next instruction as the label's target, which could be the current line
+        pendingLabels.push_back(lineVector[0]);
+        lineVector.erase(lineVector.begin());
+        if (lineVector.size() == 0) {
+          continue;
+        }
+      }
+
+      //Fill in pending labels
+      for (unsigned int labelIndex = 0; labelIndex < pendingLabels.size(); labelIndex++) {
+        (*labelAddressMap)[pendingLabels[labelIndex]] = lineAddress;
+      }
+      pendingLabels.clear();
+
+      //If the next word still isn't an instruction, give up
+      if (!instructions::mnemonicOpcodeMap.contains(lineVector[0])) {
+        std::cerr << "ERROR: Expected an instruction on line " << i + 1 \
+                  << ", got '" << lineVector[0] << "' instead" << std::endl;
+        return false;
+      }
+
+      //Add an entry for the processed data and the instruction
+      tokens->emplace_back();
+      (*tokens)[lineAddress].instruction = lineVector[0];
+      (*tokens)[lineAddress].lineNumber = i + 1;
+
+      //Check operand is present and save, if required
+      bool operandPresent = (lineVector.size() > 1) ? true : false;
+      if (instructions::mnemonicOperandMap.at(lineVector[0])) {
+        //Requires an operand, none found
+        if (!operandPresent) {
+          std::cerr << "ERROR: Missing operand for instruction '" << lineVector[0] \
+                    << "' on line " << i + 1 << std::endl;
+          return false;
+        }
+
+        (*tokens)[lineAddress].operandLabel = lineVector[1];
+      }
+
+      lineAddress++;
+    }
+
+    //Warn about remaining unresolved labels
+    for (unsigned int i = 0; i < pendingLabels.size(); i++) {
+      std::cerr << "WARNING: Couldn't give label '" << pendingLabels[i] \
+                << "' a line number, ignoring" << std::endl;
+    }
+    pendingLabels.clear();
+
+    return true;
+  }
 }
 
 /*
@@ -48,77 +133,15 @@ namespace {
 */
 int assembleProgram(SystemState* systemState, std::vector<std::string>* inputData) {
   //Variable length storage for processed data
-  struct CodeData {
-    int lineNumber;
-    std::string instruction;
-    std::string operandLabel = "NONE";
-    int operandData = 0;
-  };
   std::vector<CodeData> processedData;
 
   //Track label addresses and pending labels
   std::unordered_map<std::string, int> labelAddressMap = {{"NONE", 0}};
-  std::vector<std::string> pendingLabels;
-  int lineAddress = 0;
 
-  //Iterate over each line of code
-  for (unsigned int i = 0; i < inputData->size(); i++) {
-    //Split the line into tokens, skip comments
-    std::vector<std::string> lineVector = splitLine((*inputData)[i]);
-    if (lineVector.size() == 0) {
-      continue;
-    }
-
-    //If the first token isn't an instruction, assume it's a label
-    if (!instructions::mnemonicOpcodeMap.contains(lineVector[0])) {
-      //Use the next instruction as the label's target (could be on the current line)
-      pendingLabels.push_back(lineVector[0]);
-      lineVector.erase(lineVector.begin());
-      if (lineVector.size() == 0) {
-        continue;
-      }
-    }
-
-    //Fill in pending labels
-    for (unsigned int labelIndex = 0; labelIndex < pendingLabels.size(); labelIndex++) {
-      labelAddressMap[pendingLabels[labelIndex]] = lineAddress;
-    }
-    pendingLabels.clear();
-
-    //If the first token still isn't an instruction, give up
-    if (!instructions::mnemonicOpcodeMap.contains(lineVector[0])) {
-      std::cerr << "ERROR: Expected an instruction on line " << i + 1 \
-                << ", got '" << lineVector[0] << "' instead" << std::endl;
-      return -1;
-    }
-
-    //Add an entry for the processed data and the instruction
-    processedData.emplace_back();
-    processedData[lineAddress].instruction = lineVector[0];
-    processedData[lineAddress].lineNumber = i + 1;
-
-    //Check operand is present and save, if required
-    bool operandPresent = (lineVector.size() > 1) ? true : false;
-    if (instructions::mnemonicOperandMap.at(lineVector[0])) {
-      //Requires an operand, none found
-      if (!operandPresent) {
-        std::cerr << "ERROR: Missing operand for instruction '" << lineVector[0] \
-                  << "' on line " << i + 1 << std::endl;
-        return -1;
-      }
-
-      processedData[lineAddress].operandLabel = lineVector[1];
-    }
-
-    lineAddress++;
+  //Generate tokens and label address map
+  if (!tokeniseCode(inputData, &processedData, &labelAddressMap)) {
+    return -1;
   }
-
-  //Warn about remaining labels
-  for (unsigned int i = 0; i < pendingLabels.size(); i++) {
-    std::cerr << "WARNING: Couldn't give label '" << pendingLabels[i] \
-              << "' a line number, ignoring" << std::endl;
-  }
-  pendingLabels.clear();
 
   //Get the final size of the program
   int programLength = processedData.size();
